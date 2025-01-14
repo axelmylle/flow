af1bf68f2de385b3a881719d683d596e87a9ea17
Axel Mylle <axelmylle@gmail.com>
2025-01-14 21:58:26 +0100

    wip

--- apps/web/src/components/landing/features.tsx
+++ apps/web/src/components/landing/features.tsx
@@ -244,7 +244,7 @@ export const Features = () => (
                   <div className="flex items-center gap-3 text-neutral-800">
                     <div className="relative">
                       <img
-                        alt="Profile"
+                        alt="Pofile"
                         width="40"
                         height="40"
                         decoding="async"
