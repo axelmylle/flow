eb9aec77cb2fecbb39a835ba2acbe77431ad4f3f
Axel Mylle <axelmylle@gmail.com>
2025-01-18 12:03:22 +0100

    hi

--- apps/app/next.config.mjs
+++ apps/app/next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
   experimental: {
     instrumentationHook: process.env.NODE_ENV === "production",
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   // Add logging configuration
   logging: {
     fetches: {
